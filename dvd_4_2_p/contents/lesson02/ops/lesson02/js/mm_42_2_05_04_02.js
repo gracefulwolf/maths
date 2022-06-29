23d538206f0baa5750aa986b554c5a7f2e981dca
jaehee77 <jaiyah@naver.com>
2022-06-29 14:10:28 +0900

    [박만구 4학년] 비디오 파일 6,8,9

---
+++ dvd_4_2_p/contents/lesson02/ops/lesson02/js/mm_42_2_05_04_02.js
@@ -0,0 +1,31 @@
+
+// {volume[학기], part[단원], chapter[차시], section[코너/상단탭], tab(하단탭)}
+var PAGE_DEPTH = { vi: 0, pi: 1, ci: 5, si: 3, ti: 1 };
+// 상단 탭 -- 안 넣으면 PAGE_DEPTH.si 값으로 처리 됨
+var NAV_TAB_INDEX = 2;
+
+function initPage() {
+	console.log("initPage");
+	var corner = $(".content-container > .content").data("instance");
+
+
+	/**
+	 * 페이지 개별 스크립트 :: cc 애니메이션 재생
+	 */
+	$("#btn-play-ani").on("click", function(){
+		console.log("play ani");
+		$("#btn-play-ani").addClass("toggle");
+		$("#btn-play-ani").hide();
+
+		/**
+		 * @param {string} targetName (null or movieclip id)
+		 * @param {object} option 
+		 */
+		$("#animation-01").data("instance").play(null, {onComplete: onComplete, endStop: true, startFrame: 1});
+
+		function onComplete(mc){
+			$("#btn-play-ani").removeClass("toggle");
+			$("#btn-play-ani").show();
+		}
+	});
+};
